c20e42502189d4338c05ed7eb6ee9e08d70454d2
Nitesh1-91 <niteshtiwari486@gmail.com>
2020-07-13 10:32:38 +0530

    rate

--- MangalWebProject/Models/Entity/MagalModel.Context.cs
+++ MangalWebProject/Models/Entity/MagalModel.Context.cs
@@ -51,5 +51,6 @@ namespace MangalWebProject.Models.Entity
         public virtual DbSet<Mst_SchemePurity> Mst_SchemePurity { get; set; }
         public virtual DbSet<Mst_SchemeMaster> Mst_SchemeMaster { get; set; }
         public virtual DbSet<Mst_ProductRate> Mst_ProductRate { get; set; }
+        public virtual DbSet<Mst_ProductRateDetails> Mst_ProductRateDetails { get; set; }
     }
 }
