541b95a418415fc87282b654d5fcdd79122938dc
Nitesh1-91 <niteshtiwari486@gmail.com>
2020-07-17 17:01:04 +0530

    FINANCEVIEWM

--- MangalWebProject/Models/FinancialViewModel.cs
+++ MangalWebProject/Models/FinancialViewModel.cs
@@ -10,15 +10,18 @@ namespace MangalWebProject.Models
     {
         public int ID { get; set; }
 
-        [Required(ErrorMessage = "FinancialYear From is Required")]
+        [Required(ErrorMessage = "Financial Year From is Required")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         [DataType(DataType.DateTime)]
         public string FinancialYearFrom { get; set; }
 
-        [Required(ErrorMessage = "FinancialYear To is Required")]
+        [Required(ErrorMessage = "Financial Year To is Required")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         [DataType(DataType.DateTime)]
         public string FinancialYearTo { get; set; }
+
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
 
         public string FinacialYear { get; set; }
 
